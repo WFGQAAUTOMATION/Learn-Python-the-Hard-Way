9f0799ad4e9bc1a908e1340d2fbc53dea5443aaf
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2015-11-11 14:14:29 -0500

    tetsd

--- Exercise_21.py
+++ Exercise_21.py
@@ -25,7 +25,7 @@ iq = divide(100, 2)
 print "Age: %d, Height: %d, Weight: %d, IQ: %d \n" % (age, height, weight, iq)
 
 
-# A puzzle for the extra credit, type it in anyway.
+# A puzzle for the extra credit, type it in anyway!!!
 print "Here is a puzzle. \n"
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
