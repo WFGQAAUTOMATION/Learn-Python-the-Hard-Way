d0bb42fcfc627e493a0eb376dd55b895aa624ca3
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2015-11-16 15:17:44 -0500

    added new exercises

--- Exercise_33.py
+++ Exercise_33.py
@@ -1,13 +1,18 @@
-i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
 
-    i += 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+def print_nums(counter):
+    i = 0
+    while i < counter:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i += 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+
+print_nums(6)
 
 print "The numbers: "
 
